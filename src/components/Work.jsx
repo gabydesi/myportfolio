c2428b64d8a61ee8aff0b436b79caa3d8f98290c
Gabriela Acevedo <gabrieladesiree@gmail.com>
2023-06-29 19:02:43 -0300

    adding some space

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,7 +6,7 @@ import Nomadeando from "../assets/nomadeando.jpg";
 
 function Work() {
   return (
-    <div name="work" className="w-full md:h-screen text-[#a1bdd0] bg-[#18434e]">
+    <div name="work" className="w-full md:h-screen text-[#a1bdd0] bg-[#18434e] py-60">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-[#a1bdd0] border-[#d8ddea] ">
